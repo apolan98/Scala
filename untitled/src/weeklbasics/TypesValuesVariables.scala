454d7187af54b3becdddea9780b3dd9e0c02aa3f
hdmaster <apoloinikova@gmail.com>
2021-02-19 20:48:15 +0300

    message

---
+++ untitled/src/weeklbasics/TypesValuesVariables.scala
@@ -0,0 +1,9 @@
+package weeklbasics
+
+object TypesValuesVariables extends App {
+
+  val aString: String = "Hello"
+  println(aString)
+  val aLong = 11L
+  println(aLong.getClass) // выводит int
+}
